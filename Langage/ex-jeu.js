86843920ec9154c5d3a111d528a8d06c7f43eff4
Romain <romain@EXMacBookProMBZ.local>
2018-08-22 14:59:53 +0200

    Correction jeu

--- Langage/ex-jeu.js
+++ Langage/ex-jeu.js
@@ -1,17 +1,57 @@
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
 const readline = require('readline');
 
 const rl = readline.createInterface(process.stdin, process.stdout);
+const entierAlea = getRandomIntInclusive(0, 100);
+const essais = [];
 
 function jouer() {
+  if (essais.length) {
+    console.log('Vous avez déjà joué : ' + essais.join(' - '));
+  }
+
   rl.question('Veuillez saisir un nombre : ', (saisie) => {
 
-    console.log('Vous avez écrit : ', saisie);
+    const entierSaisi = parseInt(saisie);
 
-    jouer();
-    /* en fin de partie
+    if (isNaN(entierSaisi)) {
+      console.log('Erreur: il faut saisir un nombre');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      return jouer();
+    }
+
+    if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      return jouer();
+    }
+
+    console.log('Gagné');
     rl.close();
-    process.exit(0); // kill le process
-     */
   });
 
 }
