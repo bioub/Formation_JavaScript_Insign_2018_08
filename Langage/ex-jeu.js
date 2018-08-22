15aa0b874e8b759c501b31319b9c7045b8c8e744
Romain <romain@EXMacBookProMBZ.local>
2018-08-22 17:27:20 +0200

    Correction object

--- Langage/ex-jeu.js
+++ Langage/ex-jeu.js
@@ -1,71 +1,67 @@
-function getRandom() {
-  return Math.random();
-}
+'use strict';
 
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min +1)) + min;
-}
+const random = {
+  get: function () {
+    return Math.random();
+  },
+  getArbitrary: function (min, max) {
+    return Math.random() * (max - min) + min;
+  },
+  getInt: function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  },
+  getIntInclusive: function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+};
 
 const readline = require('readline');
 
-const rl = readline.createInterface(process.stdin, process.stdout);
-const entierAlea = getRandomIntInclusive(0, 100);
-const essais = [];
+function Jeu(options) {
+  options = options || {};
+  const min = options.min || 0;
+  const max = options.max !== undefined ? options.max : 100;
 
-function jouer() {
-  if (essais.length) {
-    console.log('Vous avez déjà joué : ' + essais.join(' - '));
+  this._rl = readline.createInterface(process.stdin, process.stdout);
+  this._entierAlea = random.getIntInclusive(min, max);
+  this._essais = [];
+}
+
+Jeu.prototype.jouer = function() {
+  if (this._essais.length) {
+    console.log('Vous avez déjà joué : ' + this._essais.join(' - '));
   }
 
-  rl.question('Veuillez saisir un nombre : ', (saisie) => {
+  this._rl.question('Veuillez saisir un nombre : ', (saisie) => {
 
     const entierSaisi = parseInt(saisie);
 
     if (isNaN(entierSaisi)) {
       console.log('Erreur: il faut saisir un nombre');
-      return jouer();
+      return this.jouer();
     }
 
-    essais.push(entierSaisi);
+    this._essais.push(entierSaisi);
 
-    if (entierSaisi < entierAlea) {
+    if (entierSaisi < this._entierAlea) {
       console.log('Trop petit');
-      return jouer();
+      return this.jouer();
     }
 
-    if (entierSaisi > entierAlea) {
+    if (entierSaisi > this._entierAlea) {
       console.log('Trop grand');
-      return jouer();
+      return this.jouer();
     }
 
     console.log('Gagné');
-    rl.close();
+    this._rl.close();
   });
 
-}
+};
 
-jouer();
-
-
-// call stack
-// ^
-// |
-// |
-// |                                     question      question
-// |                      question       jouer         jouer
-// |require - createInt - jouer    ..... =>       .... =>       ...
-// +-------------------------------------------------------------> temps
-// output :
-
+const partie = new Jeu();
+partie.jouer();
